335a353bdc460ed979f4433c261d114d743758d3
ftcaarya <rautaarya23@gmail.com>
2025-05-08 22:29:52 -0400

    Initial Commit

---
+++ ForYou Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ForYou Watch App
+//
+//  Created by Aarya Raut on 5/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForYou Watch App/ForYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForYouApp.swift
+//  ForYou Watch App
+//
+//  Created by Aarya Raut on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ForYou_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
